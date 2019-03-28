30daf2d7354e02d64aabb9697007392c04524157
zhangzhenglong <zhangzhenglong@szy.cn>
2019-03-28 20:29:24 +0800

    add readme

--- template/src/main.js
+++ template/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import pv from './mixins/pv'
 Vue.config.productionTip = false
 Vue.mixin(pv)
 new Vue({
 	render: h => h(App),
-	router
+	router,
+	store
 }).$mount('#app')
