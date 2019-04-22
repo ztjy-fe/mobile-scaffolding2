a42a019bed925252efc4a77edf3c8e4e37734f06
zhangzhenglong <zhangzhenglong@szy.cn>
2019-04-22 17:39:20 +0800

    update

--- template/vue.config.js
+++ template/vue.config.js
@@ -1,6 +1,8 @@
 // webpack配置
 module.exports = {
+	publicPath: '/',
 	assetsDir: 'static', // 打包后静态资源路径
+	productionSourceMap: false,
 	devServer: {
 		host: '0.0.0.0', // 服务启动时的主机名
 		port: 8586, // 端口号
