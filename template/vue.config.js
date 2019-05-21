25a0de4de5af548f5aeb962a7802640cd5f26e31
杨波 <yangbo@szy.cn>
2019-05-21 20:14:11 +0800

    打包配置后文件加时间戳

--- template/vue.config.js
+++ template/vue.config.js
@@ -1,6 +1,20 @@
 // webpack配置
 module.exports = {
+	publicPath: '/',
 	assetsDir: 'static', // 打包后静态资源路径
+	productionSourceMap: false,
+	configureWebpack: {
+		output: {
+			filename: 'static/js/[name].[hash:8]' + utils.getFixedDate((+new Date()), 'yyyyMMddHHmmss') + '.js',
+			chunkFilename: 'static/js/[name].[chunkhash:8]' + utils.getFixedDate((+new Date()), 'yyyyMMddHHmmss') + '.js'
+		}
+	},
+	css: {
+		extract: {
+			filename: 'static/css/[name].[hash:8]' + utils.getFixedDate((+new Date()), 'yyyyMMddHHmmss') + '.css',
+			chunkFilename: 'static/css/[id].[chunkhash:8]' + utils.getFixedDate((+new Date()), 'yyyyMMddHHmmss') + '.css'
+		}
+	},
 	devServer: {
 		host: '0.0.0.0', // 服务启动时的主机名
 		port: 8586, // 端口号
